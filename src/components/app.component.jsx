cd99321e1670d08ef9c6d229ab684254cf34ed58
MahmudulRafi <rafi.mahmudul.cse@gmail.com>
2021-08-18 22:16:33 +0600

    components modified

--- src/components/app.component.jsx
+++ src/components/app.component.jsx
@@ -1,17 +1,68 @@
-import React, { Component } from "react";
-import Navbar from "./navbar.component";
-import Counters from "./counters.component";
+import React, { Component } from 'react';
+import Navbar from './navbar.component';
+import Counters from './counters.component';
+import Reset from './reset.components';
 
 class App extends Component {
-  state = {};
-  render() {
-    return (
-      <>
-        <Navbar />
-        <Counters />
-      </>
-    );
-  }
+    state = {
+        counters: [
+            { id: 0, value: 0 },
+            { id: 1, value: 0 },
+            { id: 2, value: 0 },
+            { id: 3, value: 0 },
+            { id: 4, value: 0 },
+        ],
+    };
+
+    handleReset = () => {
+        const allCounterRest = this.state.counters.map((counter, index) => {
+            const obj = { id: index, value: 0 };
+            return obj;
+        });
+        this.setState({ counters: allCounterRest });
+    };
+
+    handleIncrement = (id) => {
+        const incrementCounters = this.state.counters.map((counter, index) => {
+            if (counter.id == id)
+                return { id: index, value: counter.value + 1 };
+            else return counter;
+        });
+        this.setState({ counters: incrementCounters });
+    };
+
+    handleDecrement = (id) => {
+        const decrementCounters = this.state.counters.map((counter, index) => {
+            if (counter.id == id && counter.value > 0)
+                return { id: index, value: counter.value - 1 };
+            else return counter;
+        });
+        this.setState({ counters: decrementCounters });
+    };
+
+    getNonZeroItems = () => {
+        let countNonZero = 0;
+        this.state.counters.forEach((counter) => {
+            if (counter.value > 0) {
+                countNonZero++;
+            }
+        });
+        return countNonZero;
+    };
+
+    render() {
+        return (
+            <>
+                <Navbar nonZeroItems={this.getNonZeroItems()} />
+                <Reset onReset={this.handleReset} />
+                <Counters
+                    counters={this.state.counters}
+                    onIncrement={this.handleIncrement}
+                    onDecrement={this.handleDecrement}
+                />
+            </>
+        );
+    }
 }
 
 export default App;
--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,18 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Navbar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Counters <span class="badge bg-secondary">1</span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
+    state = {};
+    render() {
+        return (
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        Counters{' '}
+                        <span class="badge bg-secondary">
+                            {this.props.nonZeroItems}
+                        </span>
+                    </a>
+                </div>
+            </nav>
+        );
+    }
 }
 
 export default Navbar;
